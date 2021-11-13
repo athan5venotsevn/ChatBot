429aebae2e076f3b9b765e4d0c781ad0c4d0c33e
atham5notsevn <74407637+athan5venotsevn@users.noreply.github.com>
2021-11-13 18:01:58 +0530

    Update help_me.py

--- chatrobot/plugins/help_me.py
+++ chatrobot/plugins/help_me.py
@@ -50,8 +50,8 @@ async def sedlyfsir(event):
             add_usersid_in_db(event.sender_id)
             await chatbot.send_message(Config.DUMB_CHAT, f"NEW USER ! \nUser ID : `{event.chat_id}`")
         await chatbot.send_file(event.chat_id, file=Config.CUSTOM_IMG, caption=text_me, buttons = [
-             [custom.Button.inline("💙💙Susy💙😇", data="mewant")],
-             [custom.Button.inline("Close 🔐", data="close ")],
+             [custom.Button.inline("💙💙Love You💙💙�", data="mewant")],
+             [custom.Button.inline("💙💙�Susy💙�😇", data="close ")],
               ]
              )
     
